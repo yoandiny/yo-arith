25c84c262ae58b9d71946bcc48cb834c71b42867
Yoan <yoandiny14@gmail.com>
2026-05-29 09:18:17 +0300

    fix: another package in ArithController changed

--- src/main/java/mg/yoan/yoarith/controller/ArithController.java
+++ src/main/java/mg/yoan/yoarith/controller/ArithController.java
@@ -1,6 +1,6 @@
 package mg.yoan.yoarith.controller;
 
-import mg.yoan.arith.service.ArithService;
+import mg.yoan.yoarith.service.ArithService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
